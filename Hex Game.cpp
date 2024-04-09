99f4b2fb3d485ffeafe78b1844efff284651715d
Stardust <2409602366@qq.com>
2024-04-09 11:13:26 +0800

    unified pointer representation

--- Hex Game.cpp
+++ Hex Game.cpp
@@ -9,29 +9,30 @@
 #include <ctime>
 using namespace std;
 
-double epilson = 1e-6;
+double epilson = 1e-2;
+bool isFirst = false;	//AI是先手还是后手
 
-//全局函数
 /*index值转有序对*/
 void toPos(int a, int& x, int& y) {
-		x = a / 11;
-		y = a % 11;
+	x = a / 11;
+	y = a % 11;
 }
-
 /*有序对转变为int，一一对应的*/
 int toIndex(int x, int y) {
 	return x * 11 + y;
 }
 
+
+
 class DisjointSet {
 public:
 	DisjointSet() {}
 	DisjointSet(const DisjointSet& set) {
-		for(int i = 0;i < 121;i += 1) {
+		for (int i = 0; i < 121; i += 1) {
 			father[i] = set.father[i];
 		}
 	}
-	int father[130];	//并查集数组
+	int father[121];	//并查集数组
 	/*并查集，father中数字>=0，指代该节点的父节点；father中数字<0，说明该节点为根节点，|father|为该节点所有子节点个数（包括自己）*/
 	void BuildFather(int n) {
 		for (int i = 0; i < n; i++)
@@ -59,7 +60,7 @@ public:
 	}
 
 	/*合并相连的棋子（并查集的合并）*/
-	void UnionStones(int x, int y, int board[][SIZE + 4]) {
+	void UnionStones(int x, int y, int** board) {
 		int I = toIndex(x, y);	//记录(x,y)对应的int值
 		int state = board[x][y];	//记录(x,y)的状态，方便检测合并
 		int xx = x - 1, yy = y;
@@ -83,121 +84,102 @@ public:
 	}
 };
 
-/*蒙特卡洛搜索树*/
+
+/*在棋盘下满之后，判断谁赢，return true说明AI赢，false说明RIVAL赢
+	用于随机模拟对局中判断AI输赢*/
+bool checkWinner(DisjointSet* set) {
+	vector<int> frontierRoots;
+	if (isFirst == false) {
+		for (int i = 0; i < 121; i += 11) {
+			int root = set->Find(i);
+			frontierRoots.push_back(root);
+		}
+		sort(frontierRoots.begin(), frontierRoots.end());
+		for (int i = 10; i < 121; i += 11) {
+			int root = set->Find(i);
+			if (binary_search(frontierRoots.begin(), frontierRoots.end(), root)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	else {
+		for (int i = 0; i < 11; i += 1) {
+			int root = set->Find(i);
+			frontierRoots.push_back(root);
+		}
+		sort(frontierRoots.begin(), frontierRoots.end());
+		for (int i = 110; i < 121; i += 1) {
+			int root = set->Find(i);
+			if (binary_search(frontierRoots.begin(), frontierRoots.end(), root)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
+/*蒙特卡洛搜索树中的每个节点*/
 struct MCSTNode {
 	MCSTNode* parent = nullptr;	//父节点
-
 	MCSTNode* next[121] = { nullptr };	//121个子节点，每个节点代表棋盘上一个点
-
 	int N = 0;	//当前节点随即下了多少局棋了
-
-	bool isFirst = false;
-
 	int win = 0;	//赢了多少局
-
-	int move;
-
-	DisjointSet* set;
-
-	int player = RIVAL;	//当前节点是哪个玩家下的
-
+	int move;		//当前节点是父节点得到哪一个子节点
+	DisjointSet* set;		//当前节点的并查集
+	int player = RIVAL;	//当前节点是哪个玩家下的，初始化为RIVAL（根节点是对手下的最后一颗棋）
 	int** clonedBoard;	//克隆的棋盘
+	bool isLeaf = true;		//是否是叶节点
 
-	bool isLeaf = true;
-
-	bool is_leaf()  {
-		for(int i = 0;i < 121;i += 1) {
-			if(next[i] != nullptr) 
+	bool is_leaf() {
+		for (int i = 0; i < 121; i += 1) {
+			if (next[i] != nullptr)
 				isLeaf = false;
 		}
 	}
 
 	~MCSTNode() {
-		for(int i = 0;i < 121;i += 1) {
+		for (int i = 0; i < 121; i += 1) {
 			delete next[i];
 		}
 	}
-
-	bool checkWinner(bool isFirst) {
-		vector<int> frontierRoots;
-		if(isFirst == false) {
-			for(int i = 0;i < 121;i += 11) {
-				int root = set->Find(i);
-				frontierRoots.push_back(root);
-			}
-			sort(frontierRoots.begin(),frontierRoots.end());
-			for(int i = 10;i < 121;i += 11) {
-				int root = set->Find(i);
-				if(binary_search(frontierRoots.begin(),frontierRoots.end(),root)) {
-					return true;
-				}
-			}
-			return false;
-		}
-		else {
-			for(int i = 0;i < 11;i += 1) {
-				int root = set->Find(i);
-				frontierRoots.push_back(root);
-			}
-			sort(frontierRoots.begin(),frontierRoots.end());
-			for(int i = 110;i < 121;i += 1) {
-				int root = set->Find(i);
-				if(binary_search(frontierRoots.begin(),frontierRoots.end(),root)) {
-					return true;
-				}
-			}
-			return false;
-		}
-	}
 };
+/*蒙特卡洛树搜索算法*/
 class MCTS {
 public:
-	MCSTNode* root;
+	MCSTNode* root;	//这棵博弈树的根节点，即当前输入的棋盘状态
 
-	MCTS(int** board,DisjointSet* set, bool first) {
+	/*构造MCTS，只会在HexGame.run中调用一次
+		board是棋盘状态，set是此状态下的并查集，first为AI是否为先手*/
+	MCTS(int** board, DisjointSet* set) {
 		root = new MCSTNode();
 		root->set = set;
-		root->isFirst = first;
-		root->clonedBoard = new int*[SIZE + 4];
-		for(int i = 0;i < SIZE + 4;i += 1) {
-			root->clonedBoard[i] = new int[SIZE + 4];
+		root->clonedBoard = new int* [SIZE];
+		for (int i = 0; i < SIZE; i += 1) {
+			root->clonedBoard[i] = new int[SIZE];
 		}
-		for(int i = 0;i < SIZE + 4;i += 1) {
-			for(int j = 0;j < SIZE + 4;j += 1) {
+		for (int i = 0; i < SIZE; i += 1) {
+			for (int j = 0; j < SIZE; j += 1) {
 				root->clonedBoard[i][j] = board[i][j];
 			}
 		}
+		
 	}
 
-	
-
+	/*选取进行模拟对局的节点*/
 	MCSTNode* select(MCSTNode* node) {
-		while(!node->isLeaf) {
+		while (!node->isLeaf) {
 			node = bestChild(node);
 		}
 		return node;
 	}
 
-	void expand(MCSTNode* node) {
-		for(int i = 0;i < 121;i += 1) {
-			if(node->next[i] == nullptr) {
-				node->isLeaf = false;
-				node->next[i] = new MCSTNode();
-				node->next[i]->parent = node;
-				node->next[i]->move = i;
-				node->next[i]->player = -node->player;
-				break;
-			}
-		}
-	}
-
 	MCSTNode* bestChild(MCSTNode* node) {
 		double max = -1;
 		MCSTNode* best = nullptr;
-		for(int i = 0;i < 121;i += 1) {
-			if(node->next[i] != nullptr) {
-				double UCB = node->next[i]->win / (node->next[i]->N + epilson) + sqrt(2 * log(node->N) / (node->next[i]->N + epilson)) + (rand() % 100) / 100.0 * epilson; // Add a random number between 0 and 1
-				if(UCB > max) {
+		for (int i = 0; i < 121; i += 1) {
+			if (node->next[i] != nullptr) {	//应当必不为空，在进行寻找bestchild时，节点应当121个子
+				double UCB = node->next[i]->win / (node->next[i]->N + epilson) + sqrt(2 * log(root->N + epilson) / (node->next[i]->N + epilson)) + (rand() % 100) / 100.0 * epilson; // Add a random number between 0 and 1
+				if (UCB > max) {
 					max = UCB;
 					best = node->next[i];
 				}
@@ -205,58 +187,56 @@ public:
 		}
 		return best;
 	}
-	/*反向传播*/
-	void backpropagation(MCSTNode* node, int result) {
-		while(node != nullptr) {
-			node->N += 1;
-			node->win += result;
-			node = node->parent;
-		}
-	}
-	/*模拟*/
+
+	/*模拟，当前是纯随机对局，node是进行模拟对局的节点*/
 	int simulation(MCSTNode* node) {
-		node->clonedBoard = new int*[11];
-		for(int i = 0;i < 11;i += 1) {
-			node->clonedBoard[i] = new int[11];
+		/*设置node节点状态的棋盘 和 模拟用board*/
+		int** simulation_board;
+		node->clonedBoard = new int* [SIZE];
+		simulation_board = new int* [SIZE];
+		for (int i = 0; i < SIZE; i++) {
+			node->clonedBoard[i] = new int[SIZE];
+			simulation_board[i] = new int[SIZE];
 		}
-		for(int i = 0;i < 11;i += 1) {
-			for(int j = 0;j < 11;j += 1) {
-				node->clonedBoard[i][j] = root->clonedBoard[i][j];
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				node->clonedBoard[i][j] = node->parent->clonedBoard[i][j];
+				simulation_board[i][j] = node->clonedBoard[i][j];
 			}
 		}
-		MCSTNode* temp = node;
-		node->set = new DisjointSet(*root->set);
 		int x, y;
-		toPos(temp->move, x, y);
-		node->clonedBoard[x][y] = temp->player;
-		node->set->UnionStones(x, y, reinterpret_cast<int(*)[SIZE + 4]>(node->clonedBoard));
-		while(temp->parent != nullptr) {
-			temp = temp->parent;
-			toPos(temp->move, x, y);
-			node->clonedBoard[x][y] = temp->player;
-			node->set->UnionStones(x, y, reinterpret_cast<int(*)[SIZE + 4]>(node->clonedBoard));
-		}
-		//随机下棋
-		vector<pair<int,int>> emptyGrids;
-		for(int i = 0;i < 11;i += 1) {
-			for(int j = 0;j < 11;j += 1) {
-				if(node->clonedBoard[i][j] == 0) {
-					emptyGrids.push_back(make_pair(i,j));
+		toPos(node->move, x, y);
+		node->clonedBoard[x][y] = -node->parent->player;		//= node->player
+		simulation_board[x][y] = node->clonedBoard[x][y];
+		/*设置node节点状态的并查集 和 模拟用set*/
+		DisjointSet* simulation_set;
+		node->set = new DisjointSet(*node->parent->set);
+		node->set->UnionStones(x, y, node->clonedBoard);
+		simulation_set = new DisjointSet(*node->set);
+
+		/*纯随机对局，这里就是之后添加策略所要修改的地方*/
+		vector<pair<int, int>> emptyGrids;
+		for (int i = 0; i < 11; i += 1) {
+			for (int j = 0; j < 11; j += 1) {
+				if (simulation_board[i][j] == 0) {
+					emptyGrids.push_back(make_pair(i, j));
 				}
 			}
 		}
-		while(emptyGrids.size() > 0) {
+		int who = -node->player;	//在上面构建该节点board过程中，该节点对应的那颗棋已经下完，也就是说下一颗棋（正式随即对局的第一颗棋）是-node->player下
+		while (emptyGrids.size() > 0) {
 			int index = rand() % emptyGrids.size();
 			int x = emptyGrids[index].first;
 			int y = emptyGrids[index].second;
 			emptyGrids.erase(emptyGrids.begin() + index);
-			node->clonedBoard[x][y] = node->player;
-			node->player = -node->player;
+			simulation_board[x][y] = who;
+			simulation_set->UnionStones(x, y, simulation_board);
+			who = -who;
 		}
-		//判断赢家
-		bool win = node->checkWinner(node->isFirst);
+		//判断AI输赢
+		bool win = checkWinner(simulation_set);
 		//返回结果
-		if(win) {
+		if (win) {
 			return 1;
 		}
 		else {
@@ -264,23 +244,47 @@ public:
 		}
 	}
 
-	void run(int iterations) {
-		srand(time(0));
-		while(iterations--) {
-			MCSTNode* node = select(root);
-			expand(node);
-			int win = simulation(node);
-			backpropagation(node, win);
+	/*反向传播*/
+	void backpropagation(MCSTNode* node, int result) {
+		while (node != nullptr) {
+			node->N += 1;
+			node->win += result;
+			node = node->parent;
 		}
 	}
 
+	/*对节点（以这一节点为起始模拟的对局一定已完成）扩展子节点*/
+	void expand(MCSTNode* node) {
+		node->isLeaf = false;
+		for (int i = 0; i < 121; i++) {
+			if (node->next[i] == nullptr) {		//应该是不需要
+				node->next[i] = new MCSTNode();
+				node->next[i]->parent = node;
+				node->next[i]->move = i;
+				node->next[i]->player = -(node->player);
+			}
+		}
+	}
+
+	/*蒙特卡洛自博弈开始，最终形成一棵可计算UCT的博弈树*/
+	void run(int iterations) {
+		srand(time(0));
+		while (iterations--) {
+			MCSTNode* node = select(root);
+			int win = simulation(node);
+			backpropagation(node, win);
+			expand(node);
+		}
+	}
+
+	/*决策*/
 	int getBestMove() {
 		double max = -1;
 		int best = -1;
-		for(int i = 0;i < 121;i += 1) {
-			if(root->next[i] != nullptr) {
-				double UCB = root->next[i]->win / (root->next[i]->N + epilson) + sqrt(2 * log(root->N) / (root->next[i]->N + epilson)) + (rand() % 100) / 100.0 * epilson; // Add a random number between 0 and 1
-				if(UCB > max) {
+		for (int i = 0; i < 121; i += 1) {
+			if (root->next[i] != nullptr) {
+				double UCB = root->next[i]->win / (root->next[i]->N + epilson) + sqrt(2 * log(root->N + epilson) / (root->next[i]->N + epilson)) + (rand() % 100) / 100.0 * epilson; // Add a random number between 0 and 1
+				if (UCB > max) {
 					max = UCB;
 					best = i;
 				}
@@ -292,121 +296,62 @@ public:
 
 
 
-/*int值转变为有序对，一一对应的*/
-/*
-void toPos(int a, int& x, int& y) {
-	if (a == 120) {
-		x = 10; y = 10;
-		return;
-	}
-	int units = a % 10;	//个位数
-	int tens = a / 10;		//十位数（可能是两位）
-	if (units >= tens) {
-		y = units - tens;
-		x = (a - y) / 11;
-	}
-	else {
-		int b = tens - units - 1;
-		x = units + b;
-		y = a - 11 * x;
-	}
-}
-*/
-
-
-
-
-
-
-
-
-
-
 class HexGame {
 public:
-	DisjointSet set;
-
+	DisjointSet* set;
 	MCTS* mcts;
 
 	/* board=1为红方/先手的棋，board=-1为蓝方/后手的棋 */
 	int steps = 0;	//双方已下棋回合数
-	int board[SIZE + 4][SIZE + 4];	//棋盘状态，默认均为0（未落子）
+	int **board;	//棋盘状态，默认均为0（未落子）
 public:
 	void BuildBoard() {
-		set.BuildFather(SIZE * SIZE);
+		/*初始化*/
+		set = new DisjointSet();
+		set->BuildFather(SIZE * SIZE);
+		board = new int* [SIZE];
+		for (int i = 0; i < SIZE; i++) {
+			board[i] = new int[SIZE];
+		}
+		for (int i = 0; i < SIZE; i++)
+			for (int j = 0; j < SIZE; j++)
+				board[i][j] = 0;
+		/*读入数据*/
 		scanf("%d", &steps);
 		int x, y;
-		bool first = false;
 		for (int i = 0; i < steps - 1; i++) {
 			scanf("%d%d", &x, &y);   //对方落子
 			if (i == 0 && x == -1) {
 				//输入的第一棋是（-1，-1），说明AI己方是先手，不用管
-				first = true;
+				isFirst = true;
 			}
 			else if (x != -1) {
 				board[x][y] = RIVAL;
-				set.UnionStones(x, y,board);
+				set->UnionStones(x, y, board);
 			}
 			scanf("%d%d", &x, &y);  //这里输入的绝对不可能是（-1，-1）  //己方落子
 			board[x][y] = AI;
-			set.UnionStones(x, y,board);
+			set->UnionStones(x, y, board);
 		}
 		scanf("%d%d", &x, &y);		//对方落子
 		board[x][y] = RIVAL;
-		set.UnionStones(x, y,board);
-		mcts = new MCTS(reinterpret_cast<int**>(board),&set,first);
-	}
-	
-	
-	/*判断当前随机棋局是否结束，fa是那个时候的并查集，(x,y)是新下的棋的坐标
-	* who指示新下的棋是先手方（红）还是后手方（蓝），who=1是先手红，who=0是后手蓝 */
-	bool isOver(int fa[], int x, int y, int who) {
-		int I = toIndex(x, y);
-		int root = set.Find(I);
-		if (set.father[root] <= -11) {		//若新下的棋连成一片的超过了11颗
-			bool frontier1 = false, frontier2 = false;
-			for (int i = 0; i < 121; i++) {
-				if (set.Find(i) == root || i == root) {
-					int xx, yy;
-					toPos(i, xx, yy);
-					if (who == 1) {
-						if (xx == 0)
-							frontier1 = true;
-						else if (xx == 10)
-							frontier2 = true;
-					}
-					else {
-						if (yy == 0)
-							frontier1 = true;
-						else if (yy == 10)
-							frontier2 = true;
-					}
-					if (frontier1 && frontier2)
-						return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	/*检查当前坐标是否越界*/  //应该还要写个检查坐标是否合法的函数
-	bool isValid(int x, int y) {
-		if (x < 0 || x > 10 || y < 0 || y > 10)
-			return false;
-		return true;
+		set->UnionStones(x, y, board);
+		mcts = new MCTS(board, set);
 	}
 
 	void play() {
 		BuildBoard();
 		mcts->run(1000);
 		int best = mcts->getBestMove();
-		int x,y;
+		int x, y;
 		toPos(best, x, y);
 		printf("%d %d\n", x, y);
 	}
 };
 
-int main() {
+
+
+int main(void) {
 	HexGame game;
 	game.play();
 }
